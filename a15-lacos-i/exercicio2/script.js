126e213673e34e85e8f08cad30d81be344bd4c0d
Edson <edson.exe@outlook.com>
2022-08-15 21:47:29 -0300

    a015-laços

---
+++ a15-lacos-i/exercicio2/script.js
@@ -0,0 +1,8 @@
+let tabuada = prompt('Informe um número para tabuada')
+let fim = 1
+
+do {
+  console.log(tabuada * fim)
+  fim += 1
+}
+while(fim <= 10) 
---
+++ a15-lacos-i/exercicio3/script.js
@@ -0,0 +1,5 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+for (let i in maioresPaises) {
+  console.log(`${Number(i) + 1} - ${maioresPaises[i]}`)
+}
