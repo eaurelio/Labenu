8976d67645eacc65b9e9f73c412ebb71c219e506
Edson <edson.exe@outlook.com>
2022-08-17 21:53:36 -0300

    Laços 16

---
+++ a016-template/script.js
@@ -0,0 +1,47 @@
+const filmes = [
+  {
+    titulo: 'O Auto da Compadecida',
+    ano: 2000,
+    diretor: 'Guel Arraes',
+    elenco: [
+      'Selton Mello',
+      'Mateus Nachtergaele',
+      'Marco Nanini',
+      'Fernanda Montenegro',
+    ],
+  },
+  {
+    titulo: 'Carandiru',
+    ano: 2001,
+    diretor: 'Hector Babenco',
+    elenco: [
+      'Wagner Moura',
+      'José Carlos Vasconcelos',
+      'Ailton Graça',
+      'Caio Blat',
+    ],
+  },
+  {
+    titulo: 'Aquarius',
+    ano: 2012,
+    diretor: 'Kléber Mendonça Filho',
+    elenco: [
+      'Sônia Braga',
+      'Humberto Carrão',
+      'Maeve Jinkings',
+      'Bárbara Colen',
+    ],
+  },
+]
+
+// escreva seu código abaixo 👇🏻
+
+for (let i in filmes) {
+  console.log(`Filme: ${filmes[i].titulo}. Ano: ${filmes[i].ano}. Diretor: ${filmes[i].diretor}`)
+  let elenco = 'Elenco: '
+
+  for (let j in filmes[i].elenco) {
+    elenco += `${filmes[i].elenco[j]}, `
+  }
+  console.log(elenco)
+}
--- a15-lacos-i/exercicio1/script.js
+++ a15-lacos-i/exercicio1/script.js
@@ -1,10 +1,16 @@
 let quant;
 let conta = 0;
 
+
+
 while (quant !== 'N') {
   quant = prompt('Deseja mais uma coxinha? Digite S para sim e N para não').toUpperCase()
-  conta += 1
-  console.log(conta)
+  if (quant === 'N' || quant === 'S') {
+    conta += 1
+    console.log(conta)
+  } else {
+    alert('Caractere inválido! É necessário informar S ou N!!!')
+  }
 }
 
-alert(`Você comeu ${conta} coxinhas. O valor total da conta é R$ ${conta * 2.50}`)
+alert(`Você comeu ${conta} coxinhas. O valor total da conta é R$ ${(conta * 2.50).toFixed(2)}`)
