599718dbd91958e0b23f74353b40557906f5b9aa
Edson <edson.exe@outlook.com>
2023-01-09 19:02:21 -0300

    React Router - Início

---
+++ react-router-template/src/App.js
@@ -0,0 +1,12 @@
+import { MainContainer } from "./style"
+
+
+function App() {
+  return (
+    <MainContainer>
+      <h1>Aula de React Router</h1>
+    </MainContainer>
+  )
+}
+
+export default App
---
+++ react-router-template/src/components/Header.js
@@ -0,0 +1,17 @@
+import {StyledHeader, StyledButton} from "../style"
+
+function Header() {
+
+  return (
+    <StyledHeader>
+      <StyledButton >
+        Ir para página inicial
+      </StyledButton>
+      <StyledButton >
+        Ir para página de perfil
+      </StyledButton>
+    </StyledHeader>
+  );
+}
+
+export default Header;
